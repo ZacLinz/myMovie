cdaadbd6fbe3380737002d66d53cd56f28ac7726
ZacLinz <zaclinzmeyer@gmail.com>
2019-02-25 19:04:16 -0600

    fix sever.js

--- server.js
+++ server.js
@@ -2,27 +2,34 @@ let http = require('http'),
   fs = require('fs'),
   url = require('url');
 
-http.createServer((request, response)=>{
-  var addr = request.url,
-    q = url.parse(addr, true),
-    filePath = '';
 
-  if (q.pathname.include('documentation')){
-    filePath = (__dirname + '/documentation.html');
-  } else {
+
+http.createServer((request, response) => {
+    var addr = request.url,
+      q = url.parse(addr, true),
+      filePath = '';
+
+    if (q.pathname.includes('documentation')) {
+      filePath = (__dirname + '/documentation.html');
+    } else {
       filePath = 'index.html';
-  }
-
-  fs.appendFile('log.txt', 'URL: ' + address + '\nTimestamp: ' + timeStamp + '\n\n', function(err){
-    if (err) {
-      console.log(err);
     }
-    else {
-      console.log('Added to log.');
-    }
+    fs.readFile(filePath, function(err, data) {
+      if (err) {
+        throw err;
+      }
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(data);
+      response.end();
   });
 
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + timeStamp + '\n\n', function(err){
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log('Added to log.');
+      }
+    });
 
-  response.writeHead(200, {'content-type': 'text/plaing'});
-  response.end('Hello Node!\n');
 }).listen(8080);
