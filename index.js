c017bc87bf7ca2bd51c2387530bcc3d6665e8053
ZacLinz <zaclinzmeyer@gmail.com>
2019-03-08 20:00:53 -0600

    fix endpoints

--- index.js
+++ index.js
@@ -7,40 +7,140 @@ const app = express();
 
 let topMovies =[{
   title: 'The Incredibles',
-  director: 'Brad Bird'
+  director:{
+    name: 'Brad Bird',
+    bio: 'https://www.imdb.com/name/nm0083348/bio',
+    birthYear: '1957',
+    deathYear: '',
+  },
+  genre: [' animation', ' action', ' adventure', ' family']
 },
 {
   title: 'The Mummy',
-  director: 'Alex Kurtzman'
+  director: {
+    name:'Alex Kurtzman',
+    bio:'https://www.imdb.com/name/nm0476064/',
+    birthYear: '1973',
+    deathYear: ''
+  },
+  genre: [' action', ' adventure']
 },
 {
   title: 'Batman Begins',
-  director: 'Christopher Nolan'
+  director:{
+    name: 'Christopher Nolan',
+    bio: 'https://www.imdb.com/name/nm0634240/bio',
+    birthYear: '1970',
+    deathYear: ''
+  },
+  genre: [' super hero', ' action']
 },
 {
   title: 'The Dark Knight',
-  director: 'Christopher Nolan'
+  director:{
+    name: 'Christopher Nolan',
+    bio: 'https://www.imdb.com/name/nm0634240/bio',
+    birthYear: '1970',
+    deathYear: ''
+  },
+  genre: [' super hero',  ' action']
 },
 {
   title: 'The Dark Knight Rises',
-  director: 'Christopher Nolan'
+  director:{
+    name: 'Christopher Nolan',
+    bio: 'https://www.imdb.com/name/nm0634240/bio',
+    birthYear: '1970',
+    deathYear: ''
+  },
+  genre: [' super hero', ' action']
 },
 {
   title: 'The Truman Show',
-  director: 'Peter Weir'
+  director:{
+    name: 'Peter Weir',
+    bio: 'https://www.imdb.com/name/nm0001837/bio',
+    birthYear: '1944',
+    deathYear: '',
+  },
+  genre: [ 'mystery', ' reality']
 },
 {
   title: 'What We Do in the Shadows',
-  director: 'Jemaine Clement'
+  director: {
+    name:'Jemaine Clement',
+    bio: 'https://www.imdb.com/name/nm1318596/bio',
+    birthYear: '1974',
+    deathYear:''
+  },
+  genre: [' comedy', ' super natural']
 },
 {
   title: 'Nausicaa of the Valley of the Wind',
-  director: 'Hayao Miyazaki'
+  director: {
+    name:'Hayao Miyazaki',
+    bio: 'https://www.imdb.com/name/nm0594503/bio',
+    birthYear: '1941',
+    deathYear: ''
+  },
+  genre:[' animation']
 },
 {
   title: 'The Lion King',
-  director: 'Roger Allers'
+  director: {
+    name:'Roger Allers',
+    bio: 'https://www.imdb.com/name/nm0021249/bio',
+    birthYear: '1949',
+    deathYear: ''
+  },
+  genre: [' animation', ' musical', ' animal', ' family']
 }];
+
+
+let backupDirectors =[{
+  name: 'Brad Bird',
+  bio: 'https://www.imdb.com/name/nm0083348/bio',
+  birthYear: '1957',
+  deathYear: '',
+},
+{
+  name:'Alex Kurtzman',
+  bio:'https://www.imdb.com/name/nm0476064/',
+  birthYear: '1973',
+  deathYear: ''
+},
+{
+  name: 'Christopher Nolan',
+  bio: 'https://www.imdb.com/name/nm0634240/bio',
+  birthYear: '1970',
+  deathYear: ''
+},
+{
+  name: 'Peter Weir',
+  bio: 'https://www.imdb.com/name/nm0001837/bio',
+  birthYear: '1944',
+  deathYear: '',
+},
+{
+  name:'Jemaine Clement',
+  bio: 'https://www.imdb.com/name/nm1318596/bio',
+  birthYear: '1974',
+  deathYear:''
+},
+{
+  name:'Hayao Miyazaki',
+  bio: 'https://www.imdb.com/name/nm0594503/bio',
+  birthYear: '1941',
+  deathYear: ''
+},
+{
+  name:'Roger Allers',
+  bio: 'https://www.imdb.com/name/nm0021249/bio',
+  birthYear: '1949',
+  deathYear: ''
+}]
+
+
 let users =[{
   userName: 'User_name',
   password: 'sample password',
@@ -57,24 +157,36 @@ app.use(function(err, req, res, next){
 }),
 app.use(express.static('public'));
 
-//endpoint 1
+//endpoint 1 returns a list of all movies
 app.get('/movies', function(req, res){
   res.json(topMovies)
 });
-//endpoint 2
+
+//endpoint 2 returns infomation about a single movie
 app.get('/movies/:title', function(req, res){
   res.json(topMovies.find((movie)=>{
-    return movie.title.toLowerCase() === req.params.title
+    return movie.title.toLowerCase() === req.params.title.toLowerCase()
   }));
 });
+
 //endpoint 3 add semicolon to path later
-app.get('/movies/genre', function(req, res){
-  res.send('Succesful get request about a specific genre.')
+app.get('/movies/genre/:title', function(req, res){
+  topMovies.find((movie) => {
+    if (movie.title.toLowerCase() === req.params.title.toLowerCase()){
+      return res.json(movie.genre.join(''));
+    }
+  })
 });
-//endpoint 4 need to add array on directors specifically
-app.get('/movies/director', function(req, res){
-  res.send('Succesful GET request about a director.')
+
+
+//endpoint 4 returns data about a director
+app.get('/directors/:name', function(req, res){
+  res.json(backupDirectors.find((person) => {
+    return person.name.toLowerCase() === req.params.name.toLowerCase()
+  }));
 });
+
+
 //endpoint 5
 app.post('/users', function(req, res){
   let newUser = req.body;
