58ac50a06a293a75054383e0b9a09aadd14520e0
ZacLinz <zaclinzmeyer@gmail.com>
2019-03-18 18:07:08 -0600

    add mongoose and models.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express'),
   morgan = require('morgan'),
   bodyParser = require('body-parser'),
-  uuid = require('uuid');
+  uuid = require('uuid'),
+  mongoose = require('mongoose');
+
+const Models = require('./models.js');
+const Movies = Models.Movie;
+const Users = Models.User;
+const Directors = Models.Director;
+const Genres = Models.Genre;
+
+mongoose.connect('mongodb://localhost:27017/myMovieDB', {useNewUrlParser: true});
+
 
 const app = express();
 
@@ -141,14 +151,6 @@ let backupDirectors =[{
 }]
 
 
-let users =[{
-  userName: 'User_name',
-  password: 'sample_password',
-  email: 'sample@mail.com',
-  birthday: 'mm/dd/yyyy',
-  favoriteMovies: ['the incredibles']
-}]
-
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
@@ -160,94 +162,175 @@ app.use(express.static('public'));
 
 //endpoint 1 returns a list of all movies
 app.get('/movies', function(req, res){
-  res.json(topMovies)
+  Movies.find()
+  .then(function(movies){
+    res.status(201).json(movies)
+  })
+  .catch(function(err){
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  })
 });
 
 //endpoint 2 returns infomation about a single movie
 app.get('/movies/:title', function(req, res){
-  res.json(topMovies.find((movie)=>{
-    return movie.title.toLowerCase() === req.params.title.toLowerCase()
-  }));
+  Movies.findOne({title: req.params.title})
+  .then(function(movie){
+    res.json(movie)
+  })
+  .catch(function(err){
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  })
 });
 
-//endpoint 3 add semicolon to path later
-app.get('/movies/genre/:title', function(req, res){
-  topMovies.find((movie) => {
-    if (movie.title.toLowerCase() === req.params.title.toLowerCase()){
-      return res.json(movie.genre.join(''));
-    }
+//endpoint 3 return data about a genre
+app.get('/movies/genre/:name', function(req, res){
+  Genres.find({name: req.params.name})
+  .then(function(genre){
+    res.json(genre)
+  })
+  .catch(function(err){
+    console.error(err);
+    res.status(500).send("Error: " + err);
   })
 });
 
 
 //endpoint 4 returns data about a director
 app.get('/directors/:name', function(req, res){
-  res.json(backupDirectors.find((person) => {
-    return person.name.toLowerCase() === req.params.name.toLowerCase()
-  }));
+  Directors.find({name: req.params.name})
+  .then(function(person){
+    res.json(person)
+  })
+  .catch(function(err){
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  })
 });
 
 
-//endpoint 5
+//endpoint 5 allow users to register
+/* json expected in this format
+ID : Integer,
+userName: String,
+password: String,
+email: String,
+birthday: stringn,
+*/
 app.post('/users', function(req, res){
-  let newUser = req.body;
+  Users.findOne({userName : req.body.userName})
+  .then(function(user){
+    if (user){
+      return res.status(400).send(req.body.userName + "already exists");
+    } else{
+      Users
+      .create({
+        userName: req.body.userName,
+        password: req.body.password,
+        email: req.body.email,
+        birthday: req.body.birthday
+      })
+      .then(function(user) {res.status(201).json(user) })
+      .catch(function(error){
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      })
+    }
+  }).catch(function(err){
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    })
+  });
 
-  users.push(newUser);
-  res.send('new user succesfully added!');
+
+//endpoint 6 allow users to update information by userName
+/*json like this:
+{
+  userName: String, (required),
+  password: String, (required),
+  email: String, (required),
+  birthday: Date
+}*/
+app.put('/users/:userName', function(req, res){
+  Users.update({userName: req.params.userName}, {$set:
+  {
+    userName: req.body.userName,
+    password: req.body.password,
+    email: req.body.email,
+    birthday: req.body.birthday
+  }},
+  {new: true},
+  function(err, updatedUser){
+    if(err){
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    }else{
+      res.json(updatedUser)
+    }
+  })
 })
 
 app.get('/users', (req, res)=>{
-  res.json(users);
+  Users.find()
+  .then(function(users){
+    res.status(201).json(users)
+  })
+  .catch(function(err){
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  })
+});
+
+
+
+//endpoint 7 add to a movie to a list of users favorties
+app.post('/users/:userName/favorites/:movieID', function(req, res){
+  Users.findOneAndUpdate({userName: req.params.userName},{
+    $push: {favorites: req.params.MovieID}
+  },
+  {new: true},
+  function(err, updatedUser){
+    if (err){
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    }else{
+      res.json(updatedUser)
+    }
+  })
+});
+
+//endpoint 8 allow a user to remove a movie from their list of favorites
+app.delete('/users/:userName/favorites/:movieID', function(req, res){
+  Users.findOneAndRemove({favorites: req.params.movieID})
+  .then(function(movieID){
+    if (!movieID){
+      res.status(400).send(req.params.movieID + " was not found.");
+    }else {
+      res.status(200).send(req.params.movieID + " was removed from your favorites.");
+    }
+  })
+  .catch(function(err){
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
 })
 
-//endpoint 6
-app.put('/users/:userName/:password/:email/:birthday', function(req, res){
-  users.find((user) => {
-    if (user.userName === req.params.userName){
-      users.password = req.params.password;
-      users.email = req.params.email;
-      users.birthday = req.params.birthday;
-      res.status(201).send('information succesfully updated!')
-    }
-    else
-      res.status(404).send('unable to update information');
-    })
-  })
-
-//endpoint 7
-app.put('/users/:userName/favorites/:movie', function(req, res){
-  users.find((user) => {
-    if (user.userName === req.params.userName){
-      user.favoriteMovies.push(req.params.movie);
-      res.status(201).send('movie added to favorites')
-    }
-    else{
-      res.status(400).send('unable to add movie to favorites')
+//endpoint 9 allows users to delete themselves
+app.delete('/users/:userName', function(req, res){
+  Users.findOneAndRemove({userName: req.params.userName})
+  .then(function(user){
+    if (!user){
+      res.status(400).send(req.params.userName + " was not found.");
+    } else{
+      res.status(200).send(req.params.userName + " was deleted");
     }
   })
-})
-
-//endpoint 8
-app.delete('/users/:userName/favorites/:movie', function(req, res){
-  let favorites = users.find((user)=>{return user.userName === req.params.userName})
-    if (favorites){
-      users.favoriteMovies.filter((obj) => {return obj.movie !== req.params.movie});
-      res.status(201).send('movie removed from favorites')
-    }
-    else {
-      res.status(401).send('unable to remove from favorites');
-    }
-  })
-
-//endpoint 9
-app.delete('/users/:userName/:password/deregister', function(req, res){
-  users.find((user) => {
-    if (user.userName === req.params.userName || user.password === req.params.password){
-      users.filter((obj)=>{return obj.userName !== req.params.userName})
-      res.send('user successfully deleted from app')
-    }
-  })
-})
+  .catch(function(err){
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
 
 app.get('/documentation', function(req, res){
   res.sendFile('public/documentation.html', {root: __dirname})
---
+++ models.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+var movieSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  description: {type: String, required: true},
+  genre: {
+    name: String,
+    description: String
+  },
+  director:{
+    name: String,
+    bio: String
+  },
+});
+
+var userSchema = mongoose.Schema({
+  userName: {type: String, required: true},
+  password:{type: String, required: true},
+  email:{type: String, required: true},
+  birthday: Date,
+  favorites: [{type: mongoose.Schema.Types.ObjectId, ref:'Movies'}]
+});
+
+var directorSchema = mongoose.Schema({
+  name: String,
+  bio: String,
+  birth: String,
+  death: String
+});
+
+var genreSchema = mongoose.Schema({
+  name: String,
+  description: String
+})
+
+var Movie = mongoose.model('Movie', movieSchema);
+var User = mongoose.model('User', userSchema);
+var Director = mongoose.model('Director', directorSchema);
+var Genre = mongoose.model('Genre', genreSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+module.exports.Director = Director;
+module.exports.Genre = Genre;
