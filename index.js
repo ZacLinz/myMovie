db971d779893c9a780b7e31fddc644164bb576c7
ZacLinz <zaclinzmeyer@gmail.com>
2019-05-05 20:48:09 -0600

    no message

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ const express = require('express'),
   validator = require('express-validator');
 
 const cors = require('cors');
-app.use(cors());
+var allowedOrigins = ['http://localhost:8080', 'http://localhost:3000']
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      var message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const Models = require('./models.js');
 const Movies = Models.Movie;
