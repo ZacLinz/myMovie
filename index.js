f3b07a8459400f2f0d3f3875ed8d184264494f4e
ZacLinz <zaclinzmeyer@gmail.com>
2019-05-05 20:27:40 -0600

    let CORS access from all origins

--- index.js
+++ index.js
@@ -8,19 +8,9 @@ const express = require('express'),
   passport = require('passport'),
   passportjs = require('./passport.js'),
   validator = require('express-validator');
-  
+
 const cors = require('cors');
-var allowedOrigins = ['http://localhost:8080', 'http://localhost:3000']
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      var message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 const Models = require('./models.js');
 const Movies = Models.Movie;
