669945cea826cd11d408ad3319cfdc6d238bf7c4
ZacLinz <zaclinzmeyer@gmail.com>
2019-07-06 16:08:54 -0600

    update cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const express = require('express'),
   validator = require('express-validator');
 
 const cors = require('cors');
-var allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'https://ovo1e.codesandbox.io', 'https://m4i8s.csb.dev']
+var allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'https://ovo1e.codesandbox.io', 'https://m4i8s.csb.dev', 'https://my-movie-app-108.herokuapp.com']
 app.use(cors({
   origin: function(origin, callback){
     if(!origin) return callback(null, true);
